118ca156f6b55151bb5e9fa0425896c3aa339efe
Renata Pausini <rpatricia@outlook.com>
2023-03-05 20:39:14 -0300

    Cart Updated :sparkles:

--- src/app/carrinho/carrinho.component.ts
+++ src/app/carrinho/carrinho.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-
+import { CarrinhoService } from './../carrinho.service';
+import { IProduto, IProdutoCarrinho } from './../produtos';
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-carrinho',
   templateUrl: './carrinho.component.html',
   styleUrls: ['./carrinho.component.css']
 })
-export class CarrinhoComponent {
+export class CarrinhoComponent implements OnInit {
+  itensCarrinho: IProdutoCarrinho [] = [];
+
+  constructor(
+    public carrinhoService: CarrinhoService
+  ) { }
+
+  ngOnInit(): void {
+    this.itensCarrinho = this.carrinhoService.obtemCarrinho();
+  }
+
+  removerProdutoCarrinho(produtoId: number) {
+    this.itensCarrinho = this.itensCarrinho.filter(item => item.id !== produtoId);
+
+  }
 
 }
