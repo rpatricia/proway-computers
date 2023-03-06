9be76d7ae16d55975cccbd6b131bc6ddd136afac
Renata Pausini <rpatricia@outlook.com>
2023-03-06 19:56:41 -0300

    Updated cart page

--- src/app/carrinho/carrinho.component.ts
+++ src/app/carrinho/carrinho.component.ts
@@ -1,3 +1,4 @@
+import { RouterModule} from '@angular/router';
 import { CarrinhoService } from './../carrinho.service';
 import { IProduto, IProdutoCarrinho } from './../produtos';
 import { Component, OnInit } from '@angular/core';
@@ -7,19 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carrinho.component.css']
 })
 export class CarrinhoComponent implements OnInit {
-  itensCarrinho: IProdutoCarrinho [] = [];
+  itensCarrinho: IProdutoCarrinho[] = [];
+  total = 0;
 
   constructor(
-    public carrinhoService: CarrinhoService
+    public carrinhoService: CarrinhoService,
+    // private router: Router
   ) { }
 
   ngOnInit(): void {
     this.itensCarrinho = this.carrinhoService.obtemCarrinho();
+    this.calculaTotal()
+  }
+
+  calculaTotal() {
+    this.total = this.itensCarrinho.reduce((prev, curr) => prev + (curr.preco * curr.quantidade), 0)
   }
 
   removerProdutoCarrinho(produtoId: number) {
     this.itensCarrinho = this.itensCarrinho.filter(item => item.id !== produtoId);
-
+    this.carrinhoService.removerProdutoCarrinho(produtoId);
+    this.calculaTotal();
+  }
+  comprar() {
+    alert("Parabéns, você finalizou a sua compra!");
+    this.carrinhoService.limparCarrinho();
+    // this.router.navigate(["produtos"]);
   }
 
 }
