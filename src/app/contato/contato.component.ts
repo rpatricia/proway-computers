9ae85be1d74b5af8f7506ed8e97811997a1e806b
Renata Pausini <rpatricia@outlook.com>
2023-03-08 22:03:01 -0300

    form validation

--- src/app/contato/contato.component.ts
+++ src/app/contato/contato.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contato',
@@ -8,7 +8,26 @@ import { FormBuilder } from '@angular/forms';
 })
 export class ContatoComponent implements OnInit {
   formContato = this.fb.group({
-    nome:["",]
+    nome: ["", [
+      Validators.minLength(4),
+      Validators.required
+    ]],
+    assunto: ["", [
+      Validators.minLength(10),
+      Validators.required
+    ]],
+    telefone: ["", [
+      Validators.minLength(11),
+      Validators.required
+    ]],
+    email: ["", [
+      Validators.email,
+      Validators.required
+    ]],
+    mensagem: ["", [
+      Validators.minLength(20),
+      Validators.required
+    ]]
   })
 
 
